620976a3f819574d15ea3efca02aea932b08d111
Eiji Takahashi <tak7iji@gmail.com>
2014-11-26 17:45:35 +0900

    一応クローズはしよう・・・

--- src/main/java/org/sample/springmvc/extra/SimpleTest.java
+++ src/main/java/org/sample/springmvc/extra/SimpleTest.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
@@ -22,29 +23,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/login")
 public class SimpleTest {
-    @Autowired ServletContext context;
+    @Autowired
+    ServletContext context;
 
-    @RequestMapping(method=RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public void login(Model model) {
         model.addAttribute("viewordownload", new ViewOrDownload());
     }
-    
-    @RequestMapping(method=RequestMethod.POST, params="checked=view")
+
+    @RequestMapping(method = RequestMethod.POST, params = "checked=view")
     public String view() {
         return "welcome";
     }
-    
-    @RequestMapping(method=RequestMethod.POST, params="checked=download1")
+
+    @RequestMapping(method = RequestMethod.POST, params = "checked=download1")
     public void download(HttpServletResponse response) throws IOException {
         System.out.println("Download1");
-            response.setContentType("application/octet-stream");
-            response.setHeader("Content-disposition", "attachment; filename=fileABC.txt");
-            String path = context.getRealPath("/WEB-INF/download.txt");
-            Path src = Paths.get(path);
-            Files.copy(src, response.getOutputStream());
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition",
+                "attachment; filename=fileABC.txt");
+        String path = context.getRealPath("/WEB-INF/download.txt");
+        Path src = Paths.get(path);
+        Files.copy(src, response.getOutputStream());
     }
-    
-    @RequestMapping(method=RequestMethod.POST, params="checked=download2")
+
+    @RequestMapping(method = RequestMethod.POST, params = "checked=download2")
     public ResponseEntity<String> download2() throws IOException {
         System.out.println("Download2");
         HttpHeaders headers = new HttpHeaders();
@@ -52,7 +55,10 @@ public class SimpleTest {
         headers.set("Content-Disposition", "attachment; filename=fileABC.txt");
         String path = context.getRealPath("/WEB-INF/download.txt");
         Path src = Paths.get(path);
-        String data = Files.lines(src).collect(Collectors.joining("\n"));
+        String data;
+        try (Stream<String> stream = Files.lines(src)) {
+            data = stream.collect(Collectors.joining("\n"));
+        }
         return new ResponseEntity<String>(data, headers, HttpStatus.OK);
     }
 }
