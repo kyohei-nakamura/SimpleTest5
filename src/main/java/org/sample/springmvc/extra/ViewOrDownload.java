cc28a3221feba3767546cd5bdd4a2b27fa6502bf
Eiji Takahashi <tak7iji@gmail.com>
2014-11-26 15:23:54 +0900

    再度修正

--- src/main/java/org/sample/springmvc/extra/ViewOrDownload.java
+++ src/main/java/org/sample/springmvc/extra/ViewOrDownload.java
@@ -1,19 +1,13 @@
 package org.sample.springmvc.extra;
 
 public class ViewOrDownload {
-    private String view;
-    private String download;
-    public String getView() {
-        return view;
-    }
-    public void setView(String view) {
-        this.view = view;
-    }
-    public String getDownload() {
-        return download;
-    }
-    public void setDownload(String download) {
-        this.download = download;
+    private String checked;
+
+    public String getChecked() {
+        return checked;
     }
 
+    public void setChecked(String checked) {
+        this.checked = checked;
+    }
 }
